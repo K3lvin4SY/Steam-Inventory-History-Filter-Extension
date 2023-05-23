3ed6bc7fd415a17b0feb1298a9dae279a2c6be5a
K3lvin4SY <kelvinbrahe@gmail.com>
2023-05-23 23:55:15 +0200

    progress on comunication with storage

--- js/filterStorage.js
+++ js/filterStorage.js
@@ -1,16 +1,30 @@
-function filterHandlerEdit() {
+async function filterHandlerEdit() {
 
 }
-function filterHandlerAdd() {
+async function filterHandlerAdd() {
+  console.log("add");
+  await chrome.storage.session.set({ name: "David", color: "green" });
+}
+async function filterHandlerRemove() {
 
 }
-function filterHandlerRemove() {
+
+async function updateFilterHandlerStorage() {
+  const { name, color } = await chrome.storage.session.get(["name", "color"]);
+  console.log(name);
+  console.log(color);
+}
+async function updateFilterOptionsStorage() {
 
 }
 
-function updateFilterHandlerStorage() {
-
-}
-function updateFilterOptionsStorage() {
+async function awdhauwhd() {
+  // set
+  await chrome.storage.session.set({ name: "David", color: "green" });
   
+  // get 
+  const { name, color } = await chrome.storage.session.get(["name", "color"]);
+  
+  // remove
+  await chrome.storage.session.remove(["name", "color"]);
 }
