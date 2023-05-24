f91e3cf5654f4364a852ac4caa9d1dc0d7cf1615
K3lvin4SY <kelvinbrahe@gmail.com>
2023-05-24 02:25:20 +0200

    progress on edit button functionallity

--- js/filterStorage.js
+++ js/filterStorage.js
@@ -1,7 +1,14 @@
 async function filterHandlerEdit() {
   if (!$J("#aFilter_handler_edit").hasClass("hFilter_button_unavailable")) {
-    $J('#filter_handler_storage').children().each(function() {
+
+    $J('#filter_handler_storage').children().each(async function() {
       if ($J(this).hasClass('selected')) {
+
+        const key = $J(this).text();
+        const { userFilterData } = await chrome.storage.sync.get(["userFilterData"]);
+        const dataToLoad = userFilterData[key];
+        changeFilterHanderValues(key, dataToLoad);
+
         $J(this).removeClass('selected');
       }
     })
@@ -50,13 +57,29 @@ async function updateFilterOptionsStorage() {
 
 }
 
-async function awdhauwhd() {
-  // set
-  await chrome.storage.session.set({ name: "David", color: "green" });
-  
-  // get 
-  const { name, color } = await chrome.storage.session.get(["name", "color"]);
-  
-  // remove
-  await chrome.storage.session.remove(["name", "color"]);
+function changeFilterHanderValues(label, data) {
+  // text inputs
+  $J("#aFilter_handler-label").val(label);
+  $J("#aFilter_handler-search").val(data.search);
+
+  // hard values
+  $J("#aFilter_handler-exterior").find(".input-option").each(function() {
+    if (data.exterior.includes($J(this).val())) {
+      $J(this).prop( "checked", true )
+    } else {
+      $J(this).prop( "checked", false )
+    }
+  });
+  $J("#aFilter_handler-quality").find(".input-option").each(function() {
+    if (data.quality.includes($J(this).val())) {
+      $J(this).prop( "checked", true )
+    } else {
+      $J(this).prop( "checked", false )
+    }
+  });
+
+  // liquid values
+
+  // update current list
+  updateCurrentSearchList2();
 }
